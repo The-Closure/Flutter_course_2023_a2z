b621a649e6026abd8a1bae781d0929317a768b58
AbdalazizTll <abdalaziztll992010@gmail.com>
2023-10-03 07:31:39 -0700

    question mid term

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:demo_bloc/bloc/counter_bloc.dart';
-import 'package:demo_bloc/bloc_observe.dart';
+import 'package:demo_bloc/config/bloc_observe.dart';
+import 'package:demo_bloc/model/question.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -15,81 +16,56 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: MultiBlocProvider(providers: [
+          BlocProvider(
+            create: (context) => CounterBloc(),
+          )
+        ], child: QuestionPage()));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class QuestionPage extends StatelessWidget {
+  QuestionPage({super.key});
 
-  final String title;
+  List<Question> question = [
+    Question(title: 'How Long this Tower is ?', answers: [
+      Answers(answer_title: '2', is_correct: false),
+      Answers(answer_title: '29', is_correct: true),
+      Answers(answer_title: '20', is_correct: false),
+      Answers(answer_title: '16', is_correct: false)
+    ])
+  ];
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => CounterBloc(),
-      child: Builder(builder: (context) {
-        return Scaffold(
-          appBar: AppBar(
-            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-            title: Text(widget.title),
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'You have pushed the button this many times:',
+    return Scaffold(
+      body: PageView.builder(
+        itemCount: question.length,
+        itemBuilder: (context, index) => Column(
+          children: [
+            Text(question[index].title),
+            SizedBox(
+              width: double.maxFinite,
+              height: 600,
+              child: GridView.builder(
+                itemCount: question[index].answers.length,
+                gridDelegate:
+                    SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                itemBuilder: (context, ind) => Container(
+                  child: Center(
+                    child: Text(question[index].answers[ind].answer_title),
+                  ),
                 ),
-                BlocBuilder<CounterBloc, CounterState>(
-                  builder: (context, state) {
-                    if (state is IncreasState) {
-                      return Text(state.counter.toString());
-                    } else if (state is DecreasState) {
-                      return Text(state.counter.toString());
-                    } else {
-                      return Text("zero counter");
-                    }
-                  },
-                )
-              ],
+              ),
             ),
-          ),
-          floatingActionButton: SizedBox(
-            width: 200,
-
-            child: Row(
-              children: [
-                FloatingActionButton(
-                  onPressed: () {
-                    context.read<CounterBloc>().add(Increment());
-                  },
-                  tooltip: 'Increment',
-                  child: const Icon(Icons.add),
-                ),
-                FloatingActionButton(
-                  onPressed: () {
-                    context.read<CounterBloc>().add(Decrement());
-                  },
-                  tooltip: 'decreament',
-                  child: const Icon(Icons.minimize),
-                ),
-              ],
-            ),
-          ), // This trailing comma makes auto-formatting nicer for build methods.
-        );
-      }),
+          ],
+        ),
+      ),
     );
   }
 }
